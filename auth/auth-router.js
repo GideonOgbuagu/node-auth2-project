7d73921edd226d2e910c08d23c24cad62924ce8f
Gideon <gideon@mac-1s-mbp.myfiosgateway.com>
2020-04-21 18:27:36 -0400

    added code to generate authentication token

---
+++ auth/auth-router.js
@@ -0,0 +1,69 @@
+const router = require("express").Router();
+const bcrypt  = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const Users = require('../users/users-model.js');
+const secrets = require('../api/secrets.js');
+
+
+router.post('/register', (req, res) => {
+    const user = req.body;
+
+    const rounds = process.env.HASH_ROUNDS || 12;
+
+    const hash = bcrypt.hashSync(user.password, rounds)
+
+    //update the user password to hashed
+
+    user.password = hash;
+
+    Users.add(user)
+         .then(stored => {
+             res.status(201).json(stored)
+         })
+         .catch(err => {
+             console.log(err);
+             res.status(500).json({errorMessage: err.message})
+         })
+})
+
+
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+
+    //update the user password to hashed
+
+    Users.findBy({ username })
+         .then(([user]) => {
+             //if we find the user, then also  check that the password match
+             if (user && bcrypt.compareSync(password, user.password)) {
+              const token = generateToken(user);  
+                res.status(201).json({ message: `Welcome, ${user.username}`, token })
+             } else {
+                 res.status(401).json({ message: "You are cannot pass! "})
+             }
+         })
+         .catch(err => {
+             console.log(err);
+             res.status(500).json({errorMessage: err.message})
+         })
+})
+
+function generateToken(user) {
+    // the data
+    const payload = {
+        userId: user.id,
+        username: user.username,
+        department: user.department
+    };
+    const secret = secrets.jwtSecret;
+    const options = {
+        expiresIn: "1 day",
+    };
+
+    return jwt.sign(payload, secret, options)
+}
+
+
+module.exports = router;
---
+++ auth/authenticator.js
@@ -0,0 +1,7 @@
+const jwt = require("jsonwebtoken");
+
+const secrets = require('../api/secrets.js');
+
+module.exports = (req, res, next) => {
+    // tokens sent as the Authorization header
+}
